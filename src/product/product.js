902cd3365e156d5e39e43942966f013f79d0e205
Omri.BenYair <omri.benyair@cyberark.com>
2022-11-09 13:18:53 +0200

    fix producer

--- src/product/product.js
+++ src/product/product.js
@@ -2,7 +2,7 @@ class Product {
     constructor(id, type, name, version, color) {
         this.id = id;
         this.type = type;
-        // this.name = name;
+        this.name = name;
         this.version = version;
         this.color = color;
     }
